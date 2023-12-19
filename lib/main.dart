33edd2d21bcaffd2072eddaf324923ec1e94fa8c
nurazizahfebriyana <azizahfebriyana@gmail.com>
2023-12-19 09:29:09 +0700

    edit import

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:pustaring/models/book.dart';
-import 'package:pustaring/ulasan/screens/bookDetailPage.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:pustaring/ulasan/screens/bookListPage.dart';
-import 'package:pustaring/ulasan/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
