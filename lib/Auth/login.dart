1604b158f4a40ccfb6a59c5492452f1c782e7dfb
rhaken <rhakenss@gmail.com>
2023-12-20 14:13:55 +0700

    coba deploy

--- lib/Auth/login.dart
+++ lib/Auth/login.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:js_interop';
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
